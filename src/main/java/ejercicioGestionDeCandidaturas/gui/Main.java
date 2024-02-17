b77cb71c32b9f32651e7c478e85eaa7fba7425fa
Luis Quintano <149412452+Bigg69Cobra@users.noreply.github.com>
2024-02-17 16:15:13 +0100

    changes

--- src/main/java/ejercicioGestionDeCandidaturas/gui/Main.java
+++ src/main/java/ejercicioGestionDeCandidaturas/gui/Main.java
@@ -10,13 +10,16 @@ import java.util.Calendar;
 
 public class Main {
     public static void main(String[] args) {
+        //controllers:
         UserController userController = new UserController();
         CompanyController companyController = new CompanyController();
+
+        //objetos:
         User user1 = new User("Perico", "pericoPec@example.com", "El de los palotes", 666777);
         User user2 = userController.createUser("John Doe", "jdoe@asdf.asdf", "asdf", 123456);
         //LaboralExperience laboralExperience1 = new LaboralExperience("Barrendero", );
 
-
+        //metodos:
         userController.createUser(user1);
         userController.addSkill(user1, "asdf");
         userController.addSkill(user1, new Skill("skill2"));
